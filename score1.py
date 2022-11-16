1c8daebdfa7d0631a401b183b2165c2a059be97a
Ejpulver <pul15004@byui.edu>
2022-11-16 09:00:03 -0700

    added score1 class

---
+++ score1.py
@@ -0,0 +1,13 @@
+
+class Score1(Drawable):
+    def __init__(self, output_service):
+        super().__init__(output_service)
+        self._value = 0
+        self.update_score(self.value)
+        self.set_font_size(25)
+    def display_score(self):
+        print(f'Score: {self.value}')
+    def update_score(self, points):
+        self.value += points
+
+
