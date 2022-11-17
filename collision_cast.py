52037ce6dfec923e273b0468248bfb2cda21f283
KillerK1998 <kaler.sappington@gmail.com>
2022-11-17 10:30:07 -0700

    changes added

--- collision_cast.py
+++ collision_cast.py
@@ -1,10 +1,12 @@
-def is_close(self, x, otherx):
-        is_close = False
-        precision = 25
-        lower_limit = otherx - precision
-        upper_limit = otherx + precision
-        
-        if self.is_close(int(collision_p.x), int(player_p.x)) and self.is_close(int(collision_p.y), int(player_p.y)):
-            if lower_limit <= x <= upper_limit:
-                is_close = True
-            return is_close
+class collision:
+
+    def is_close(self, x, otherx):
+            is_close = False
+            precision = 25
+            lower_limit = otherx - precision
+            upper_limit = otherx + precision
+            
+            if self.is_close(int(collision_p.x), int(player_p.x)) and self.is_close(int(collision_p.y), int(player_p.y)):
+                if lower_limit <= x <= upper_limit:
+                    is_close = True
+                return is_close
