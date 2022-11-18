a5545381d9c15db870df1664c202639b63b7ba1c
ChandlerNorth24 <chandlernorth@byui.edu>
2022-11-17 23:35:45 -0700

    Revert "orginization helps"
    This reverts commit e1dde9f74252187224d61af9bebf338b81d8b986.

--- Cycle/scripting/move_actors_action.py
+++
@@ -1,8 +0,0 @@
-from scripting.action import Action
-
-class MoveActorsAction(Action):
-    def execute(self, cast):
-        actors = cast.get_all_actors()
-
-        for actor in actors:
-            actor.move_next()
